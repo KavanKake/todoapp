8ecfd644a379b3e4307294b9e4af0630c06bda6b
Kavin Lokeswaran <153284678+KavanKake@users.noreply.github.com>
2026-01-07 09:52:09 +0100

    logo i navbar
    Jeg har lagt til logoen i navbaren, og akkurat nå er det bare logoen vår på siden

--- todoapp/src/routes/+layout.svelte
+++ todoapp/src/routes/+layout.svelte
@@ -1,1 +1,15 @@
+<nav>
+    <div class="content">
+        <div class="logo-side">
+            <img class="logo" src="/assets/doquire_logo.svg" alt="Doquire Logo" />
+        </div>
+    </div>
+
+</nav>
+
+
+
+
+
+
 <slot />
